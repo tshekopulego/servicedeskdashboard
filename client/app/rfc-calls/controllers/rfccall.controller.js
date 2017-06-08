ffa4a2db2ba2a3b370feacd21f8823585e054205
felily <felily@workmail.co.za>
2017-06-08 09:47:53 +0200

    Filter on RFC-Call
    added the filter on roc calls dashboard

--- client/app/rfc-calls/controllers/rfccall.controller.js
+++ client/app/rfc-calls/controllers/rfccall.controller.js
@@ -7,12 +7,77 @@ angular.module('serviceDeskApp')
     $scope.currentPage = 1;
     $scope.pageSize = 10;
     
+    
+    
+    $http.get('/api/request-type').success(function (requesttypes) {
+               requesttypes.unshift({
+                   requesttypeName: 'All',
+                   _id: -1
+               });
+               $scope.requesttypes = requesttypes;
+           });
+    
+    
+    
+    $http.get('/api/evaluation-outcome').success(function (evaluationoutcomes) {
+               evaluationoutcomes.unshift({
+                  evaluationoutcomeName: 'All',
+                   _id: -1
+               });
+               $scope.evaluationoutcomes = evaluationoutcomes;
+           });
+    
+    
 
     $http.get('/api/rfc-calls').success(function(rfccalls) {
         $scope.rfccalls = rfccalls;
         socket.syncUpdates('rfccall', $scope.rfccalls,function(event,rfccall,rfccalls){
         });
     });
+    
+    
+     $scope.searchRfccall = function (changerequesttype, callevaluationoutcome) {
+
+            if ((changerequesttype == "-1") && (callevaluationoutcome == "-1")) { //get all records
+                $http.get('/api/rfc-calls').success(function (rfccalls) {
+                    $scope.rfccalls = rfccalls;
+                    console.log('/api/rfc-calls/');
+                });
+
+            } else {
+
+                if ((changerequesttype != "-1" && !changerequesttype) && (callevaluationoutcome != "-1" && !callevaluationoutcome)) {
+                    $http.get('/api/rfc-calls/' + changerequesttype + '/' + callevaluationoutcome).success(function (rfccalls) {
+
+                        $scope.rfccalls = rfccalls;
+                    });
+                } else {
+
+                    if (changerequesttype != "-1" && !angular.isUndefined(changerequesttype)) {
+
+                        $http.get('/api/rfc-calls/' + changerequesttype + '/requesttypes').success(function (rfccalls) {
+
+                            $scope.rfccalls = rfccalls;
+
+                        });
+
+                    } else if (callevaluationoutcome != "-1") {
+
+                        $http.get('/api/rfc-calls/' + callevaluationoutcome + '/callEvaluationOutcomes').success(function (rfccalls) {
+
+                            $scope.rfccalls = rfccalls;
+
+                        });
+                    }
+
+                }
+
+            }
+        };
+    
+
+    
+    
 
     $scope.open = function (rfccall) {
 
